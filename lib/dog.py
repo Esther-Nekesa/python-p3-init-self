127df88171a95033347a739d38a602db982678b5
Esther nekesa <esthernekesasi32@gmail.com>
2025-11-20 12:24:03 +0300

    Implement __init__ and attributes for Dog and Person classes

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
 
 class Dog:
+    #  __init__ with Name and Breed defaulting to "Mutt"
+    def __init__(self, name, breed="Mutt"):
+        self.name = name
+        self.breed = breed
     pass
